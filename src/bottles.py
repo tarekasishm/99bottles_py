6fc49138fa1e3b8d33a79cbeeba65a0e44dd5a81
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-07-27 09:21:54 +0200

    Init 99 bottles kata in python

---
+++ src/bottles.py
@@ -0,0 +1,16 @@
+class Bottles:
+    def verse(
+        self,
+        verse_number: int,
+    ) -> str:
+        pass
+
+    def verses(
+        self,
+        max: int,
+        min: int,
+    ) -> str:
+        pass
+
+    def song(self) -> None:
+        pass
